3052f8a26c40ced48090598a942c4b4e71470e23
bahadirkaratas <58609158+bahadirkaratas@users.noreply.github.com>
2023-03-05 15:32:06 +0300

    UI Development

--- VR/Assets/Scripts/Client.cs
+++ VR/Assets/Scripts/Client.cs
@@ -17,9 +17,9 @@ public class Response
 public class Client
 {
     private static readonly string API_URL = "https://scary-verse.com";
+    private static string AccessToken = "ya29.a0AVvZVspZAn7mgqo7sTV86DaXEyd7dEge4cRUK1-NAb_hSa4gHoPTg0MbRPiTB0hPjYrKzncDMTkf_vISrqiF-dI9X-_6261wcwWrnrcdUA-WfFW1Bw0Jrgk93aIvQwJQOkQ05ZkC3m-EtxdYmDg7uRRB2UQlaCgYKATASARASFQGbdwaI2XheDVpT7XE0xPbI_oElFw0163";
+    private static string IdToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjI1NWNjYTZlYzI4MTA2MDJkODBiZWM4OWU0NTZjNDQ5NWQ3NDE4YmIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzNTIzNzYwNDY3NTktNDRmb2pqZ2djZjlrZXZ0ZGd2Y284ZXNlOTI5ZGVjODkuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIzNTIzNzYwNDY3NTktNDRmb2pqZ2djZjlrZXZ0ZGd2Y284ZXNlOTI5ZGVjODkuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTcwOTkwOTg4MzIzNTE0Njg0NjgiLCJlbWFpbCI6InNjYXJ5dmVyc2UudmlzaW9uZXllQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiNW1aNHF6anlkZEVGZzlkYVhIRUFiZyIsIm5hbWUiOiJWaXNpb24gRXllIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FHTm15eGF2dGRCSWxoYm9ESUJ4Sl92LWI4ZGJlakpoVVRyS1N4eThOYlVCPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IlZpc2lvbiIsImZhbWlseV9uYW1lIjoiRXllIiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2NzgwMTY1MDMsImV4cCI6MTY3ODAyMDEwM30.X9-8S-4_wrDRrvnk7wwdfHJjHeTa1eEPUMkCRo2AN8eh3LGXCmHp_mn3xp7uvG00Pfw5-rz9TpofhN2CJ6cQqF18znCiBQ5nhp9yHdRYHVHblsSRvB4O4cQGJTg-JIweKDoop4GIccZdzSMIthMEE-NLfAY3MciibWeg18dcYvoK7QbPSh0IqhJQ8K3LJPC-HiZxEeqXDnRHr8sOvCJhs6MSZjj1gOHFUPso-yp2RbvadDG7RQa2cu5LaxjlknYxeA3ne7TyEooB-3rD_rk-mIUY_6uB5-JG-CrEq0YoBf4QRC9msd6v04q-knnZeW9wG0ZtJqJu9_MHs3i16sEgrA";
 
-    private static string AccessToken { get; set; }
-    private static string IdToken { get; set; }
 
     public static void SetTokens(string access, string id)
     {
@@ -29,7 +29,14 @@ public class Client
 
     public static UnityWebRequest PrepareRequest(string method, string endpoint, object body = null)
     {
-        UnityWebRequest request = new UnityWebRequest(API_URL + endpoint);
+        string url = API_URL + endpoint;
+        if (AccessToken != null && IdToken != null)
+        {
+            url += "?access_token=" + AccessToken;
+            url += "&id_token=" + IdToken;
+        }
+
+        UnityWebRequest request = new UnityWebRequest(url);
         request.downloadHandler = new DownloadHandlerBuffer();
         request.method = method;
 
@@ -44,12 +51,6 @@ public class Client
             string json = JsonConvert.SerializeObject(body);
             byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
             request.uploadHandler = new UploadHandlerRaw(jsonToSend);
-        }
-
-        if (AccessToken != null && IdToken != null)
-        {
-            request.SetRequestHeader("access_token", AccessToken);
-            request.SetRequestHeader("id_token", IdToken);
         }
 
         return request;
---
+++ VR/Assets/Scripts/HomeScreen.cs
@@ -0,0 +1,212 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+class PastTeamResponse {
+
+    [JsonProperty("id")]
+    public string id;
+    [JsonProperty("name")]
+    public string name;
+    [JsonProperty("totalScoreGained")]
+    public string totalScoreGained;
+    [JsonProperty("minutesPlayed")]
+    public string minutesPlayed;
+
+}
+
+class Me {
+    static string displayName;
+    static string avatarUrl;
+}
+class InvitationResponse {
+
+
+    [JsonProperty("teamName")]
+    public string teamName;
+    [JsonProperty("sender")]
+    public string sender;
+}
+
+class Player {
+    [JsonProperty("playerId")]
+    public string playerId;
+    [JsonProperty("displayName")]
+    public string displayName;
+    [JsonProperty("avatarUrl")]
+    public string avatarUrl;
+    [JsonProperty("isCaptain")]
+    public bool isCaptain;
+
+}
+
+class Sender {
+
+    [JsonProperty("playerId")]
+    public string playerId;
+    [JsonProperty("displayName")]
+    public string displayName;
+    [JsonProperty("avatarUrl")]
+    public string avatarUrl;
+
+}
+class CreateTeamResponse
+{
+    [JsonProperty("success")]
+    public bool success;
+    [JsonProperty("teamId")]
+    public string teamId;
+}
+
+class AnswerInvitation {
+    [JsonProperty("id")]
+    public string id;
+    [JsonProperty("status")]
+    public string status;
+
+}
+
+public class HomeScreen : MonoBehaviour
+{
+    public PastTeam pastTeam;
+    public Invitation invitationObj;
+    Me myself;
+
+
+
+    public Button createNewGameButton;
+    public GameObject teamContainer;
+
+    private void OnCreateNewGameButtonClick()
+    {
+        StartCoroutine(CreateNewGame_Coroutine());
+    }
+
+    IEnumerator GetPastTeams_Coroutine()
+    {
+        UnityWebRequest request = Client.PrepareRequest("GET", "/teams");
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+
+            Response response = Client.GetResponseValue(request);
+            List<PastTeamResponse> teams = JsonConvert.DeserializeObject<List<PastTeamResponse>>(response.data.ToString());
+            for (int i = 0; i < teams.Count; i++)
+            {
+                PastTeamResponse team = teams[i];
+                if (pastTeam != null)
+                {
+
+                    var row = Instantiate(pastTeam, teamContainer.transform).GetComponent<PastTeam>();
+                    row.name.text = team.name;
+                    row.totalScoreGained.text = team.totalScoreGained;
+                    row.minutesPlayed.text = team.minutesPlayed;
+                    row.id = team.id;
+
+                }
+            }
+        }
+        /*else {
+            for (int i = 0; i < 2; i++)
+            {
+                if (pastTeam != null)
+                {
+
+                    var row = Instantiate(pastTeam, teamContainer.transform).GetComponent<PastTeam>();
+                    row.name.text = "team.name";
+                    row.totalScoreGained.text = "aloscore";
+                    row.minutesPlayed.text = "alominute";
+                    row.id = "idit";
+
+                }
+            }
+
+        }*/
+    }
+
+    
+
+    IEnumerator GetMyInfo_Coroutine()
+    {
+
+        UnityWebRequest request = Client.PrepareRequest("GET", "/players/me");
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+
+            Response response = Client.GetResponseValue(request);
+            myself = JsonConvert.DeserializeObject<Me>(response.data.ToString());
+            //name and avatar will be taken from here.
+            
+        }
+        else Debug.Log(request.result);
+
+    }
+
+    IEnumerator CreateNewGame_Coroutine()
+    {
+        UnityWebRequest request = Client.PrepareRequest("GET", "/teams/create");
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Response response = Client.GetResponseValue(request);
+            CreateTeamResponse createTeamResponse = JsonConvert.DeserializeObject<CreateTeamResponse>(response.data.ToString());
+
+            if (createTeamResponse.success == true)
+            {
+                GameObject createNewGameScreen = GameObject.Find("CreateNewGameScreen");
+                gameObject.SetActive(false);
+                createNewGameScreen.transform.GetChild(0).gameObject.SetActive(true);
+            }
+            else {
+                throw new Exception("New Game could not be created!");
+            }
+        }
+
+    }
+
+    IEnumerator GetInvitations_Coroutine() {
+
+        UnityWebRequest request = Client.PrepareRequest("GET", "/invitations");
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Response response = Client.GetResponseValue(request);
+            List<InvitationResponse> invitations = JsonConvert.DeserializeObject<List<InvitationResponse>>(response.data.ToString());
+
+            for (int i = 0; i < invitations.Count; i++)
+            {
+                InvitationResponse invitation = invitations[i];
+                if (invitationObj != null)
+                {
+                    var row = Instantiate(invitationObj, transform).GetComponent<Invitation>();
+                    row.teamName.text = invitation.teamName;
+                    row.sender.text = invitation.sender;
+                    
+                }
+            }
+        }
+
+    }
+    void Start()
+    {
+        //createNewGameButton.onClick.AddListener(OnCreateNewGameButtonClick);
+        StartCoroutine(GetPastTeams_Coroutine());
+        //StartCoroutine(CreateNewGame_Coroutine());
+        //StartCoroutine(GetInvitations_Coroutine());
+    }
+
+   
+}
+
+
+    
+
