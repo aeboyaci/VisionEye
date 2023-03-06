77bc1a1be0175c90dc0e18ad063bc3ef2045b5f0
Ahmet Eren BOYACI <ahmeterenboyaci45@gmail.com>
2023-03-06 19:28:12 +0300

    add game detail information to API

--- backend/src/routers/game.ts
+++ backend/src/routers/game.ts
@@ -8,6 +8,19 @@ type GameInformation = {
   roomId: string;
 };
 
+type GameDetailInformation = {
+  roomName: string;
+  minutesPlayed: number;
+  score: number;
+  achievements: {
+    id: string;
+    name: string;
+    description: string;
+    score: number;
+    owner: string;
+  }[];
+};
+
 const router = express.Router();
 
 router.post("/", enforceAuthentication, async (req, resp, next) => {
@@ -97,6 +110,55 @@ router.post("/:gameId", enforceAuthentication, async (req, resp, next) => {
     return resp.status(200).json({
       success: true,
       data: "game updated successfully",
+    });
+  } catch (ex) {
+    return resp.status(500).json({
+      success: false,
+      error: ex,
+    });
+  }
+});
+
+router.get("/:gameId", enforceAuthentication, async (req, resp, next) => {
+  try {
+    const player = req.user!;
+    const { gameId } = req.params;
+
+    const result: { values: GameDetailInformation[] } = await database.$queryRaw`
+      WITH
+        score_information as (
+          SELECT DISTINCT ON (player_id) game_id, sum(score) as score FROM player_has_achievements
+        INNER JOIN achievement ON player_has_achievements.achievement_id = achievement.id
+        INNER JOIN game ON player_has_achievements.game_id = game.id
+      WHERE player_id = ${player.id}
+      GROUP BY player_id, game_id
+        ),
+        room_information as (
+      SELECT "name", game.id as game_id FROM room
+        INNER JOIN game ON room.id = game.room_id
+        ),
+        achievement_information as (
+      SELECT game.id as game_id, achievement.id, "name", description, player.display_name as owner FROM game
+        INNER JOIN player_has_achievements ON game.id = player_has_achievements.game_id
+        INNER JOIN player ON player_has_achievements.player_id = player.id
+        INNER JOIN achievement ON player_has_achievements.achievement_id = achievement.id
+        )
+      SELECT DISTINCT ON (game.id) json_build_object(
+        'roomName', room_information."name",
+        'minutesPlayed', extract(MINUTE FROM age(ended_at, started_at)),
+        'score', score_information.score,
+        'achievements', json_agg(achievement_information)
+        ) as "values" FROM game
+        INNER JOIN score_information ON game.id = score_information.game_id
+        INNER JOIN room_information ON score_information.game_id = room_information.game_id
+        INNER JOIN achievement_information ON score_information.game_id = achievement_information.game_id
+      WHERE game.id = ${gameId}
+      GROUP BY game.id, room_information."name", ended_at, started_at, score_information.score
+    `;
+
+    return resp.status(200).json({
+      success: true,
+      data: result,
     });
   } catch (ex) {
     return resp.status(500).json({
