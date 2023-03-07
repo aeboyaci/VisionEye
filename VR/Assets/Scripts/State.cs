31fb46e349af62ece902054046dca0cb57f5a27a
Ahmet Eren BOYACI <ahmeterenboyaci45@gmail.com>
2023-03-07 12:38:47 +0300

    set starting screen to Authentication screen

--- VR/Assets/Scripts/State.cs
+++ VR/Assets/Scripts/State.cs
@@ -1,12 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class State
 {
     public static string PlayerId { get; set; }
     public static string DisplayName { get; set; }
     public static string AvatarUrl { get; set; }
     public static string ActiveTeamId { get; set; }
+    public static string ActiveGameId { get; set; }
     public static bool IsCaptain { get; set; }
 }
