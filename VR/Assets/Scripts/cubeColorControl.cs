b37aae33931c607bfc7257374c1175e2ba29e681
kadircagan <kdrcgneren@gmail.com>
2023-04-14 13:27:40 +0300

    doors mechanics and puzzles connection

--- VR/Assets/Scripts/cubeColorControl.cs
+++ VR/Assets/Scripts/cubeColorControl.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class cubeColorControl : MonoBehaviour
 {
 
     public static string cubePassForDoor="";
-    // Start is called before the first frame update
-
-
+    [SerializeField] public TMPro.TMP_Text textOfChest;
+    public string answer = "";
+    public GameObject door;
+    private void Start()
+    {
+        for(int i =0; i < 5; i++)
+        {
+            float randomNumber = Random.Range(0, 3);
+            randomNumber = (int)randomNumber;
+            if (randomNumber == 0)
+            {
+                answer = answer + "A";
+            }
+            if (randomNumber == 1)
+            {
+                answer = answer + "B";
+            }
+            if (randomNumber == 2)
+            {
+                answer = answer + "C";
+            }
+        }
+        textOfChest.text = answer;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -16,9 +38,10 @@ public class cubeColorControl : MonoBehaviour
         {
             cubePassForDoor = cubePassForDoor.Substring(cubePassForDoor.Length - 5);
         }
-        if (cubePassForDoor == "ABCBA")
+        if (cubePassForDoor == answer)
         {
             GetComponent<Renderer>().material.color = new Color(0, 204, 102);
+            door.GetComponent<XRGrabInteractable>().enabled = true;
         }
     }
 }
