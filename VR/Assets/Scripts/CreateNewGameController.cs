bce35e6d608d3169a2185e7637462cee879041f2
Ahmet Eren BOYACI <ahmeterenboyaci45@gmail.com>
2023-04-27 08:27:03 +0300

    rename scene

--- VR/Assets/Scripts/CreateNewGameController.cs
+++ VR/Assets/Scripts/CreateNewGameController.cs
@@ -200,7 +200,7 @@ public class CreateNewGameController : MonoBehaviour
             GameCreateResponse gameCreateResponse = JsonConvert.DeserializeObject<GameCreateResponse>(response.data.ToString());
             ScaryVerseState.ActiveGameId = gameCreateResponse.gameId;
 
-            SceneManager.LoadScene("bahadir");
+            SceneManager.LoadScene("Demo_01");
         }
     }
 
@@ -249,7 +249,7 @@ public class CreateNewGameController : MonoBehaviour
 
                 if (teamGameStatus.hasStarted)
                 {
-                    SceneManager.LoadScene("bahadir");
+                    SceneManager.LoadScene("Demo_01");
                 }
             }
             else if (request.result != UnityWebRequest.Result.InProgress && request.result != UnityWebRequest.Result.Success)
